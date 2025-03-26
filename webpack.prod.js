c5b11ad15cb31225b1199fc6ab7f544f7d9a2495
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-03-26 08:09:31 +0100

    chore: initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+// webpack.prod.js
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    filename: "main.[contenthash].js", // Cache busting
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+});
